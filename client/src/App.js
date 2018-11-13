210e6c557a5ecf19acd54cf6d62731cdce05656e
cleartime <562606139@qq.com>
2018-11-13 23:04:02 +0800

    init

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        <ul>
+          <li>
+            <span>
+              当前价格
+            </span>
+            <span>
+            </span>
+          </li>
+          <li>
+            <span>
+            </span>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default App;
